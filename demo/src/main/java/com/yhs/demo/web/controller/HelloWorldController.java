91a7a21d794da7f057ebc86c5a5d2a541858610a
yanghaosen <196076565@qq.com>
2017-12-11 13:56:49 +0800

    引入logback

--- demo/src/main/java/com/yhs/demo/web/controller/HelloWorldController.java
+++ demo/src/main/java/com/yhs/demo/web/controller/HelloWorldController.java
@@ -1,13 +1,36 @@
 package com.yhs.demo.web.controller;
 
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yhs.demo.properties.BusinessProperties;
 
 @RestController
 public class HelloWorldController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+	
+	@Resource
+	private BusinessProperties businessProperties;
+	
+	/*@Resource 
+	private Environment env;*/
+	
 	@RequestMapping("say")
 	public User say(){
+		logger.debug("year=" + businessProperties.getYear());
+		logger.info("moth=" + businessProperties.getMonth());
+		logger.error("day=" + businessProperties.getDay());
+		/*System.out.println("year=" + businessProperties.getYear());
+		System.out.println("moth=" + businessProperties.getMonth());
+		System.out.println("day=" + businessProperties.getDay());*/
+		/*System.out.println("year=" + env.getProperty("year"));
+		System.out.println("moth=" + env.getProperty("aaa.month"));
+		System.out.println("day=" + env.getProperty("day"));*/
 		return new User("ttt",3,6);
 	}
 	
